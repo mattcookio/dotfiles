ee5cb0a14774387554c761c22f3b9699db74ba48
Matt Cook <matt@studiosapiens.io>
2025-04-02 22:26:30 -0500

    Update lazy git commands

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -15,6 +15,9 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-        { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        { "<leader>gG", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit Current File" },
+        { "<leader>gf", "<cmd>LazyGitFilter<cr>", desc = "LazyGit Filter" },
+        { "<leader>gF", "<cmd>LazyGitFilterCurrentFile<cr>", desc = "LazyGit Filter Current File" }
     }
 }
