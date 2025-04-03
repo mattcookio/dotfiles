6479d2eb5da45e33943c8ca320b1d10818ef12f9
Matt Cook <matt.cook@brightway.com>
2025-04-03 15:35:10 -0500

    Fix

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/bin/zsh
 
 # Create necessary directories
-mkdir -p ~/.config/nvim
-mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/karabiner
+mkdir -p ~/.config
 
 # Remove existing symlinks if they exist
-rm -f ~/.config/nvim/init.lua
-rm -f ~/.config/alacritty/alacritty.yml
-rm -f ~/.config/karabiner/karabiner.json
+rm -rf ~/.config/nvim
+rm -rf ~/.config/alacritty
+rm -rf ~/.config/karabiner
 
-# Create new symlinks
-ln -s "$(pwd)/nvim/init.lua" ~/.config/nvim/init.lua
-ln -s "$(pwd)/alacritty/alacritty.yml" ~/.config/alacritty/alacritty.yml
-ln -s "$(pwd)/karabiner/karabiner.json" ~/.config/karabiner/karabiner.json
+# Create new symlinks for entire directories
+ln -s "$(pwd)/nvim" ~/.config/nvim
+ln -s "$(pwd)/alacritty" ~/.config/alacritty
+ln -s "$(pwd)/karabiner" ~/.config/karabiner
 
-echo "Configuration files have been symlinked successfully!"
+echo "Configuration directories have been symlinked successfully!"
