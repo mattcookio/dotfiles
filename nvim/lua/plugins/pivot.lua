d953258f6d6be348394b69d4118d38d1de37605e
Matt Cook <matt@studiosapiens.io>
2025-04-07 23:32:26 -0500

    Add terminal stuff

--- nvim/lua/plugins/pivot.lua
+++ nvim/lua/plugins/pivot.lua
@@ -1,5 +1,6 @@
 return {
-  'mattcookio/pivot.nvim',
+  dir = '/Users/mc/code/pivot.nvim', -- Point to local development directory
+  -- name = 'pivot.nvim',             -- Usually inferred by lazy.nvim
   config = function()
     require('pivot').setup()
   end,
