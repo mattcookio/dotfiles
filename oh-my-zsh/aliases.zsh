bd7a99e692eb3393bbb096ec427b38e0a6e91af3
Matt Cook <matt@studiosapiens.io>
2025-07-14 10:04:14 -0500

    Add a couple of aliases

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -5,3 +5,5 @@ alias yeet="git commit -n --allow-empty -m"
 # Development tools
 alias code='code-insiders'
 alias claude="~/.claude/local/claude"
+alias dkill="docker stop $(docker ps -q) && docker rm $(docker ps -aq)"
+alias dcu="docker-compose up -d"
